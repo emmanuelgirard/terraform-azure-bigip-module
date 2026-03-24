936c1cbf6b09d598a26a445aefacd3cbbaaab678
Emmanuel Girard <e.girard@f5.com>
2026-03-23 23:42:00 -0400

    feat(examples): add 3nic static IP + CFE example scaffold
    Add variables, resource group, and availability set files for the new
    bigip_azure_3nic_deploy_staticip_cfe example. Configures Canada Central region
    with specific subnet CIDRs and managed availability set (fault_domain_count=2).

---
+++ examples/bigip_azure_3nic_deploy_staticip_cfe/variables.tf
@@ -0,0 +1,39 @@
+variable "prefix" {
+  description = "Prefix for resources created by this module"
+  type        = string
+  default     = "tf-azure-bigip"
+}
+
+variable "location" {}
+
+variable "AllowedIPs" {}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "cidr" {
+  description = "Azure VNet CIDR encompassing all subnets"
+  type        = string
+  default     = "10.9.46.0/23"
+}
+
+variable "mgmt_subnet_prefix" {
+  description = "CIDR prefix for the management subnet"
+  type        = string
+  default     = "10.9.47.16/29"
+}
+
+variable "external_subnet_prefix" {
+  description = "CIDR prefix for the external subnet"
+  type        = string
+  default     = "10.9.46.0/24"
+}
+
+variable "internal_subnet_prefix" {
+  description = "CIDR prefix for the internal subnet"
+  type        = string
+  default     = "10.9.47.0/28"
+}
